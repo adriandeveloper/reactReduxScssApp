5bbe26f6315728e275b982135f0f7a93815ea864
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-09-27 01:57:22 -0500

    removes history-api-callback from build scripts

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -19,23 +19,18 @@ module.exports = {
     loaders: [
       { test: /\.(js|jsx|es6)$/,
         use: 'babel-loader',
-        exclude: /node_modules/, },
-      {
-        test: /\.(sass|scss)$/,
-        use: 'style-loader', },
-      {
-        test: /\.scss$/,
+        exclude: /node_modules/ },
+      { test: /\.(sass|scss)$/,
+        use: 'style-loader' },
+      { test: /\.scss$/,
         loader: 'css-loader',
         query: {
           modules: true,
           localIdentName: '[name]_[local]_[hash:base64:5]',
           sourceMap: true,
-          url: false,
-        }},
-      {
-        test: /\.(sass|scss)$/,
-        use: 'sass-loader',
-      }
+          url: false }},
+      { test: /\.(sass|scss)$/,
+        use: 'sass-loader' }
     ]
   },
   plugins: [
